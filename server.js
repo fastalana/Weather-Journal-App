219f6bbddcff7c8f7e47ac95993828e4cf6874d1
Alana Bellucci <fastalana@gmail.com>
2020-07-01 15:54:58 -0600

    Setting up the project environment. Including Express, Body Parser, and CORs. Creating and testing a server that runs on port 3000. Adding a console.log() to the server callback function to test the server is running as expected.

--- server.js
+++ server.js
@@ -2,37 +2,35 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* Dependencies */
+
+// Configure Express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
-// Setup Server
-
-// Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
-
-// Start up an instance of app
-
-/* Dependencies */
-/* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
-
-// Initialize the main project folder
-
 // Spin up the server
+const port = 3000;
+const server = app.listen(port, listening);
+
 // Callback to debug
+function listening(){
+    console.log('Server is Running!');
+    console.log(port);
+    console.log(server);
+}
 
 // Initialize all route with a callback function
 
