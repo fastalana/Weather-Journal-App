47ac47af559a290b00d0567e2f376927fbf433c5
Alana Bellucci <fastalana@gmail.com>
2020-07-02 13:39:10 -0600

    Troubleshooting API calls via updates to the getWeatherData async function.

--- website/app.js
+++ website/app.js
@@ -7,8 +7,10 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 // Personal API Key for OpenWeatherMap API
 const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '9812f495c39edfda37f91527ab04b2e4';
+const countryCode = ',us&appid=';
 // Sample API call:
 // api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
+// http://api.openweathermap.org/data/2.5/weather?zip=11355,us&appid=9812f495c39edfda37f91527ab04b2e4
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
@@ -20,20 +22,20 @@ function performAction(event){
     const feelings = document.getElementById('feelings').value;
 
     getWeatherData(baseURL, zip, apiKey)
-        .then(function(data){
-            postWeatherData('/add', {temperature: data.main.temperature, date: newDate, content})
-        })
+        // .then(function(data){
+        //     postWeatherData('/add', {temperature: data.main.temperature, date: newDate, content})
+        // })
 }
 
 // Function to GET Web API Data
 const getWeatherData = async (baseURL, zip, apiKey) => {
-    const response = await fetch(baseURL + zip + apiKey);
+    const response = await fetch(baseURL + zip + countryCode + apiKey);
     try{
         const data = await response.json();
         return data;
     } 
     catch(error){
-        console.log('error', error);
+        console.log("error", error);
     }
 }
 
