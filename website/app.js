4a8aab60bd43d336460c81edef13b6ae0fc61454
Alana Bellucci <fastalana@gmail.com>
2020-07-02 10:48:17 -0600

    Acquiring credentials from OpenWeatherMap and adding apiKey and baseURL as global variables.

--- website/app.js
+++ website/app.js
@@ -5,6 +5,10 @@ let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
+const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '9812f495c39edfda37f91527ab04b2e4';
+// Sample API call:
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 
 // Event listener to add function to existing HTML DOM element
 
